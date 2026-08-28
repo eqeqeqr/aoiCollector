7a0fe56cbe6970a956754cab3715521151d73f7a
hxw <1500292505@qq.com>
2026-08-28 09:41:49 +0800

    fix: 用 *.amap.com 覆盖全部高德子域名(含www.amap.com)

--- aoiCollectorJs/background.js
+++ aoiCollectorJs/background.js
@@ -231,7 +231,7 @@ async function handleSaveAOI(data) {
 
   // 通知 content script 更新面板
   try {
-    const tabs = await chrome.tabs.query({ url: ['https://gaode.com/*', 'https://*.gaode.com/*', 'https://ditu.amap.com/*', 'https://map.amap.com/*'] });
+    const tabs = await chrome.tabs.query({ url: ['https://gaode.com/*', 'https://*.gaode.com/*', 'https://amap.com/*', 'https://*.amap.com/*'] });
     for (const tab of tabs) {
       chrome.tabs.sendMessage(tab.id, { type: 'SAVED', msg }).catch(() => {});
     }
